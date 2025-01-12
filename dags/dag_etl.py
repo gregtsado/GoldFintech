38f8051ba5c9d905d8b5f29745785f9592d8364f
Tsado Gregory <50801345+gregtsado@users.noreply.github.com>
2025-01-12 14:58:35 +0000

    Update dag_etl.py

--- dags/dag_etl.py
+++ dags/dag_etl.py
@@ -2,34 +2,20 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from extract import fetch_data
-from transform import transform
+from data_quality_checks import data_quality
+from transfrom_pan import transform_fun
 from load import load_csv_to_postgres, get_postgres_engine
 
 # Get the Postgres engine
 engine = get_postgres_engine()
 
-# Define a function to debug the environment
-def debug_environment():
-    import sys
-    import subprocess
-    import os
-
-    print("Python Path:", sys.path)
-    print("Installed Packages:")
-    subprocess.run(["pip", "list"], check=True)
-
-    print("Environment Variables:")
-    for key, value in os.environ.items():
-        print(f"{key}: {value}")
-
 # Define the default arguments
 default_args = {
     'owner': 'greg',
     'start_date': datetime(year=2024, month=11, day=13),
-    'email': 'gregorytsado@gmail.com',
-    'email_on_failure': False,
+    'email': 'gregstyles68@gmail.com',
+    'email_on_failure': True,
     'email_on_retry': False
 }
 
@@ -39,18 +25,13 @@ with DAG(
     default_args=default_args,
     description='ETL pipeline for gold fintech',
     schedule_interval='0 0 * * 4',
-    catchup=True
+    catchup=False
 ) as dag: 
 
     # Start task
     start_task = DummyOperator(
         task_id='Start_pipeline'
-    )
 
-    # Debug task to check the environment
-    debug_task = PythonOperator(
-        task_id='debug_environment',
-        python_callable=debug_environment
     )
 
     # Extract task
@@ -58,24 +39,18 @@ with DAG(
         task_id='extract',
         python_callable=fetch_data
     )
+    
 
     # Transform task
-    transform_task = SparkSubmitOperator(
-        task_id='transform_task',
-        application='transform.py',  # Path to your Spark notebook or script
-        name='goldfintech-transform',
-        conn_id='spark_default',  # Ensure this Airflow connection is configured
-        application_args=[],  # Add arguments if needed
-        verbose=True,
-        conf={
-            'spark.driver.memory': '1g',
-            'spark.executor.memory': '1g',
-        },
-        env_vars={
-            'JAVA_HOME': '/opt/bitnami/java',
-            'SPARK_HOME': '/opt/bitnami/spark',
-        }
+    data_quality_task = PythonOperator(
+        task_id='data_quality_checks',
+        python_callable=data_quality
+    )
 
+    # Transform task
+    transform_task = PythonOperator(
+        task_id='transform_pan',
+        python_callable=transform_fun
     )
 
     # Load task
@@ -91,4 +66,4 @@ with DAG(
     )
 
     # Set up task dependencies
-    start_task >> debug_task >> extract_task >> transform_task >> load_task >> end_task
+    start_task >>  extract_task >> data_quality_task >> transform_task >>load_task >> end_task
